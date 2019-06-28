b6c2e8c59d7130a4ef88f3f6f258c4faa89587ce
Joffily Ferreira <joffily.ferreira@scielo.org>
2019-06-28 18:02:43 -0300

    WIP: Proposta de posicionamento e ajustes no logo

--- application/views/home.php
+++ application/views/home.php
@@ -11,47 +11,57 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <!-- ./alert -->
 
 <div class="container">
+	<div class="row">
+		<header class="header-container">
+			<div class="menu-lang">
+				<ul>
+					<li class="info">
+						<a href="<?= $about_menu_item['link'] ?>"><?= $about_menu_item['text'] ?></a>
+					</li>
+					<?php foreach ($available_languages as $available_language) : ?>
+						<li>
+							<a href="<?= $available_language['link'] ?>"><?= $available_language['language'] ?></a>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+			</div>
+		</header>
+	</div>
+
 
 	<!-- capa -->
-	<section class="cover">
-		<div class="menu-lang">
-			<ul>
-				<li class="info">
-					<a href="<?= $about_menu_item['link'] ?>"><?= $about_menu_item['text'] ?></a>
-				</li>
-				<?php foreach ($available_languages as $available_language) : ?>
-				<li>
-					<a href="<?= $available_language['link'] ?>"><?= $available_language['language'] ?></a>
-				</li>
-				<?php endforeach; ?>
-			</ul>
-		</div>
-		<h1 class="logo">
-			<span>Scientific Electronic Library Online</span>
-		</h1>
-		<form name="searchForm" id="searchForm" action="<?= SCIELO_SEARCH_URL ?>" method="get">
-			<input type="hidden" name="lang" id="lang" value="<?= $language ?>">
-			<input type="hidden" name="count" id="count" value="15">
-			<input type="hidden" name="from" id="from" value="0">
-			<input type="hidden" name="output" id="output" value="site">
-			<input type="hidden" name="sort" id="sort" value="">
-			<input type="hidden" name="format" id="format" value="summary">
-			<input type="hidden" name="fb" id="fb" value="">
-			<input type="hidden" name="page" id="page" value="1">
+	<div class="row">
+		<section class="cover">
+			<h1 class="scielo-logo" title="Scientific Electronic Library Online">
+				<a href="/">
+					<img src="/static/images/logo-scielo-portal.svg" alt="Scientific Electronic Library Online">
+				</a>
+			</h1>
+			<form name="searchForm" id="searchForm" action="<?= SCIELO_SEARCH_URL ?>" method="get">
+				<input type="hidden" name="lang" id="lang" value="<?= $language ?>">
+				<input type="hidden" name="count" id="count" value="15">
+				<input type="hidden" name="from" id="from" value="0">
+				<input type="hidden" name="output" id="output" value="site">
+				<input type="hidden" name="sort" id="sort" value="">
+				<input type="hidden" name="format" id="format" value="summary">
+				<input type="hidden" name="fb" id="fb" value="">
+				<input type="hidden" name="page" id="page" value="1">
 
-			<div class="search-box">
-				<input type="text" name="q" placeholder="<?= lang('search_placeholder') ?>" autofocus>
-				<a href="javascript:$('#searchForm').submit();" class="btn btn-default btn-input"></a>
-				<a href="<?= SCIELO_ADVANCED_SEARCH_URL ?>"><?= lang('search_link_text') ?></a>
-			</div>
-		</form>
-	</section>
-	<!-- ./capa -->
+				<div class="search-box">
+					<input type="text" name="q" placeholder="<?= lang('search_placeholder') ?>" autofocus>
+					<a href="javascript:$('#searchForm').submit();" class="btn btn-default btn-input"></a>
+					<a href="<?= SCIELO_ADVANCED_SEARCH_URL ?>"><?= lang('search_link_text') ?></a>
+				</div>
+			</form>
+		</section>
+		<!-- ./capa -->
+	</div>
+
 
 	<!-- tabs -->
 	<?php $this->load->view('partials/tabs'); ?>
 	<!-- ./tabs -->
-	
+
 </div>
 
 <!-- footer -->
--- application/views/partials/language-menu.php
+++ application/views/partials/language-menu.php
@@ -1,22 +1,26 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 <header>
 	<div class="container">
-		<div class="menu-lang">
-			<ul>
-				<li class="info">
-					<a href="<?= $about_menu_item['link'] ?>"><?= $about_menu_item['text'] ?></a>
-				</li>
-				<?php foreach ($available_languages as $available_language) : ?>
-				<li>
-					<a href="<?= $available_language['link'] ?>"><?= $available_language['language'] ?></a>
-				</li>
-				<?php endforeach; ?>
-			</ul>
+		<div class="row">
+			<header class="header-container">
+				<div class="menu-lang">
+					<ul>
+						<li class="info">
+							<a href="<?= $about_menu_item['link'] ?>"><?= $about_menu_item['text'] ?></a>
+						</li>
+						<?php foreach ($available_languages as $available_language) : ?>
+							<li>
+								<a href="<?= $available_language['link'] ?>"><?= $available_language['language'] ?></a>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+				<h1 class="logo-interno">
+					<a href="<?= base_url($language) ?>"></a>
+				</h1>
 		</div>
-		<h1 class="logo-interno">
-			<a href="<?= base_url($language) ?>"></a>
-		</h1>
 	</div>
+</header>
 </header>
